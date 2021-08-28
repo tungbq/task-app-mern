311bd7cecac94ede8302d978a1e7036da11dcd1d
Tung Bui <tungbq.info@gmail.com>
2021-08-28 11:52:20 +0700

    Add Logout button

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -5,10 +5,15 @@ import { CredentalsContext } from '../App';
 import Todos from '../components/Todos'
 
 export default function Welcome() {
-	const [credentials] = useContext(CredentalsContext);
+	const [credentials, setCredentials] = useContext(CredentalsContext);
+
+	const logout = () => {
+		setCredentials(null)
+	}
 
 	return (
 		<div>
+			{credentials && <button onClick={logout}>Logout</button>}
 			<h1>Welcome {credentials && credentials.username}</h1>
 			{!credentials && <Link to='/register'>Register</Link>}
 			<br />
