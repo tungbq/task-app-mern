f7ff32dfa1e9b403e7124b6560d86ecd528c80ab
Tung Bui <tungbq.info@gmail.com>
2021-08-28 17:19:57 +0700

    [UI] Update welcome page

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { CredentalsContext } from '../App';
 import Todos from '../components/Todos'
+import Login from './Login';
 
 export default function Welcome() {
 	const [credentials, setCredentials] = useContext(CredentalsContext);
@@ -14,10 +15,11 @@ export default function Welcome() {
 	return (
 		<div>
 			{credentials && <button onClick={logout}>Logout</button>}
-			<h1>Welcome {credentials && credentials.username}</h1>
-			{!credentials && <Link to='/register'>Register</Link>}
+			<h1>Welcome {credentials ? credentials.username : "To Task App"}</h1>
+			{/* {!credentials && <Link to='/register'>Register</Link>}
 			<br />
-			{!credentials && <Link to='/login'>Login</Link>}
+			{!credentials && <Link to='/login'>Login</Link>} */}
+			{!credentials && <Login />}
 			<br />
 			{credentials && <Todos />}
 		</div>
