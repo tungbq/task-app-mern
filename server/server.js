93b3fb43fedb7afb363f583a187e62517e63a5e9
Tung Bui <tungbq.info@gmail.com>
2021-08-29 21:04:02 +0700

    Fix deploy issue (1)

--- server/server.js
+++ server/server.js
@@ -24,8 +24,8 @@ app.use('/', todoRoutes);
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '..','client', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => {
         res.send('API is running...')
